efa8d307595e9ebdf8d542aed83a7fcb326a5221
ducadete <139269030+ducadete@users.noreply.github.com>
2025-03-29 19:56:06 +0000

    Build gerado em dist e copiado para docs

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   base: '/patient-visit-dashboard/',  
   build: {
-    outDir: 'docs',                  
+    outDir: 'dist',                 
   },
   server: {
     host: "::",
