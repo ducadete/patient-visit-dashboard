b1e681ed0e9005d04cd5f8c4f77494c04a9faaae
ducadete <139269030+ducadete@users.noreply.github.com>
2025-03-30 23:12:37 +0000

    Remoção do BrowserRouter duplicado; ajustes finais para Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import { VisitProvider } from "./contexts/VisitContext";
 import { ProfessionalProvider } from "./contexts/ProfessionalContext";
@@ -79,9 +79,7 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
               <AppRoutes />
-            </BrowserRouter>
           </TooltipProvider>
         </VisitProvider>
       </ProfessionalProvider>
